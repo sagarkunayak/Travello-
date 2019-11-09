bb8760756bf50423edf559ead5f9e2f3cc60a107
sagarkunayak <sagarkunayak104@gmail.com>
2019-11-09 09:29:26 +0530

    Travello
    Travello

---
+++ test/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sagarkumarnayak/Desktop/django_by_nr/test/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ test/projects/telusko/travello/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from . models import Destination
+
+# Create your views here.
+
+
+def index(request):
+    # dest1 = Destination()
+    # dest1.name = 'Mumbai'
+    # dest1.desc = 'The city that never sleeps'
+    # dest1.img = 'destination_1.jpg'
+    # dest1.price = 700
+    # dest1.offer = False
+
+    # dest2 = Destination()
+    # dest2.name = 'Hyderabad'
+    # dest2.desc = 'First Biriyani, Then Sherwani'
+    # dest2.img = 'destination_2.jpg'
+    # dest2.price = 650
+    # dest2.offer = True
+
+    # dest3 = Destination()
+    # dest3.name = 'Banglore'
+    # dest3.desc = 'The city of Technologies'
+    # dest3.img = 'destination_3.jpg'
+    # dest3.price = 750
+    # dest3.offer = False
+
+    # dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
+
+    return render(request, 'index.html', {'dests': dests})
+# def index1(request):
+#     return render(request,'contact.html')
